3e3a674ba6baa1ee51b53c4a10e1a72f0d9a8d49
up227 <up2277017@myport.ac.uk>
2026-06-23 12:04:52 +0100

    all pay rule implementation

--- contest/__init__.py
+++ contest/__init__.py
@@ -28,13 +28,18 @@ class Subsession(BaseSubsession):
 
 class Group(BaseGroup):
     prize = models.CurrencyField()
+    csf = models.StringField()
 
     def setup_round(self):
+        #initialization occurs with setup round
         self.prize = C.PRIZE
+        self.csf = "allpay"
+        #here we can change the type of csf!
         for player in self.get_players():
             player.setup_round()
 
-    def determine_outcome(self):
+    def determine_outcome_share(self):
+        #share rule
         total = 0
         for player in self.get_players():
             total += player.tickets_purchased
@@ -44,10 +49,27 @@ class Group(BaseGroup):
                 player.prize_won = player.tickets_purchased/total
             except ZeroDivisionError:
                 player.prize_won = 1/len(self.get_players())
+            
+
+    def determine_outcome_allpay(self):
+        for player in self.get_players():
+            if player.tickets_purchased > player.coplayer.tickets_purchased:
+                player.prize = 1
+            elif player.tickets_purchased < player.coplayer.tickets_purchased:
+                player.prize = 0
+            else:
+                player.prize = 0.5
+
+    def determine_outcome(self):
+        if self.csf == "share":
+            self.determine_outcome_share()
+        elif self.csf == "allpay":
+            self.determine_outcome_allpay()
+        for player in self.get_players():
             player.earnings = (
-                player.endowment -
-                player.tickets_purchased + player.cost_per_ticket +
-                self.prize * player.prize_won
+                player.endowment
+                - player.tickets_purchased * player.cost_per_ticket
+                + self.prize * player.prize_won
             )
             if self.subsession.is_paid:
                 player.payoff = player.earnings
