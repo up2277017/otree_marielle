03a5f3c51ee4e06746062f383a5e10c4a30cefc8
up227 <up2277017@myport.ac.uk>
2026-06-23 14:18:51 +0100

    random matching

--- contest/__init__.py
+++ contest/__init__.py
@@ -29,9 +29,16 @@ class Subsession(BaseSubsession):
             self.setup_paid_rounds()
         for group in self.get_groups():
             group.setup_round()
+        self.setup_groups()
+        for group in self.get_groups():
+            group.setup_round()
+
+    def setup_groups(self):
+        self.group_randomly()
+
     def setup_paid_rounds(self):
         for rd in random.sample(self.in_rounds(1, C.NUM_ROUNDS),
-                                k = C.NUM_PAID_ROUNDS:
+                                k = C.NUM_PAID_ROUNDS):
             rd.is_paid = True
 
 
