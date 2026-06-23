fecf7b5b977ebb8f3e05dc1fc97f06dbc4da02d9
up227 <up2277017@myport.ac.uk>
2026-06-23 10:34:24 +0100

    implement share rule

--- contest/__init__.py
+++ contest/__init__.py
@@ -32,12 +32,32 @@ class Group(BaseGroup):
         for player in self.get_players():
             player.setup_round()
 
+    def determine_outcome(self):
+        total = 0
+        for player in self.get_players():
+            total += player.tickets_purchased
+        #total = sum(player.tickets_purchased for player in self.get_player())
+        for player in self.get_players():
+            try:
+                player.prize_won = player.tickets_purchased/total
+            except ZeroDivisionError:
+                player.prize_won = 1/len(self.get_players())
+            player.earnings = (
+                player.endowment -
+                player.tickets_purchased + player.cost_per_ticket +
+                self.prize * player.prize_won
+            )
+
 
 class Player(BasePlayer):
     endowment = models.CurrencyField()
     cost_per_ticket = models.CurrencyField()
     tickets_purchased = models.IntegerField()
     prize_won = models.FloatField()
+    earnings = models.CurrencyField()
+    # earnings for that specific round, which may not be the one selected for that round.
+    # payoff is what will actually be paid to participants at the end of the experiment.
+    #payoff is already defined in otree
 
     def setup_round(self):
         self.endowment = C.ENDOWMENT
@@ -67,7 +87,12 @@ class Decision(Page):
 #this two fields almost always will be there. tickets_purchase will become Tickets Purchased automatically.
 
 class WaitForDecisions(WaitPage):
-    pass
+    wait_for_all_groups = True
+
+    @staticmethod
+    def after_all_players_arrive(subsession):
+        for group in subsession.get_groups():
+            group.determine_outcome()
 
 class Outcome(Page):
     pass
